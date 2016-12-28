d7b0b6b3427958600dc179e3d0295f31708158b0
Angelo Bovino <angelo.bovino@outlook.com>
2016-12-27 22:02:07 -0500

    requiring necessary dependencies

---
+++ config/authentication/passport-github.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+
+/*
+  Add github strategy for Authentication
+*/
